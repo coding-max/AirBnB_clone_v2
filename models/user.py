7e01bb0cb90622d2422ea7a709583f234e6557d8
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-08-10 19:44:35 +0000

    updated map (task 8)

--- models/user.py
+++ models/user.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
