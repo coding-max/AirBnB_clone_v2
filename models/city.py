e6e1cbe4db71e3a66bb19464005c3f09099453d4
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-08-10 18:57:05 +0000

    updated map (task 6)

--- models/city.py
+++ models/city.py
@@ -3,12 +3,11 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy.sql.schema import ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel
 from models.base_model import Base
 from models.city import City
-from models import storage
-from sqlalchemy.sql.schema import ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -13,15 +11,15 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
     if getenv('HBNB_TYPE_STORAGE') == "db":
-        cities = relationship("City", backref="states")
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
     else:
         @property
         def cities(self):
             cities_list = []
-            all_cities = storage.all(City)
+            all_cities = models.storage.all(City)
             for city in all_cities.values():
                 if self.id == city.state_id:
                     cities_list.append(city)
