63f34de7b1c42319515e88210bc9f3c99cfd8038
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-08-10 20:58:09 +0000

    updated relationship user.places & user.reviews

--- models/user.py
+++ models/user.py
@@ -11,16 +11,10 @@ from os import getenv
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = relationship("Place", cascade="delete", backref="user")
-        reviews = relationship("Review", cascade="delete", backref="user")
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade="delete", backref="user")
+    reviews = relationship("Review", cascade="delete", backref="user")
