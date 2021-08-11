70f1d9694de483ab6a8076c06ceeaf18f2df5adf
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-08-11 17:31:13 +0000

    BOOM!

--- models/base_model.py
+++ models/base_model.py
@@ -22,12 +22,14 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                setattr(self, "id", str(uuid.uuid4()))
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -50,7 +52,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         if "_sa_instance_state" in dictionary:
-            del dictionary["_sa_instance_state"]
+            dictionary.pop("_sa_instance_state")
         return dictionary
 
     def delete(self):
--- models/place.py
+++ models/place.py
@@ -12,11 +12,13 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-association = Table("place_amenity", Base.metadata,
-                    Column('place_id', String(60), ForeignKey('places.id'),
-                    primary_key=True, nullable=False),
-                    Column('amenity_id', String(60), ForeignKey('amenities.id',
-                    primary_key=True, nullable=False)))
+place_amenity = Table("place_amenity", Base.metadata,
+                    Column("place_id", String(60),
+                            ForeignKey("places.id"),
+                            primary_key=True, nullable=False),
+                    Column("amenity_id", String(60),
+                            ForeignKey("amenities.id"),
+                            primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -34,7 +36,7 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     reviews = relationship("Review", cascade="all,delete", backref="place")
-    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
+    amenities = relationship("Amenity", secondary="place_amenity", backref="places")
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
