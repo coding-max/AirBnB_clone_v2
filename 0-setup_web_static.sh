33b3bef7c0d1fbfc19c55ea65c98007cd0951aa6
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-08-26 23:46:08 +0000

    updated task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,22 +9,20 @@ mkdir -p /data/web_static/releases/test/
 chown -R ubuntu:ubuntu /data/
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-echo "This shit it's working" > /data/web_static/releases/test/index.html
-echo "Holberton School" > /var/www/html/index.html
-
-printf %s "<!doctype html5>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>404 error</title>
-</head>
-<body>
-    <!-- get out of here pastrana -->
-    <img style=\"display: block; margin: auto; margin-top: 150px;\" src=\"https://images.squarespace-cdn.com/content/v1/51cdafc4e4b09eb676a64e68/1470175715831-NUJOMI6VW13ZNT1MI0VB/image-asset.jpeg?format=300w\" alt=\"Girl in a jacket\" height=\"300\">
-    <h2 style=\"display: block; text-align: center; font-size: xx-large; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; margin-bottom: 0;\">404 PAGE NOT FOUND</h2>
-    <h3 style=\"display: block; text-align: center; font-size: large; font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; margin-top: 5px;\">Ceci n'est pas une page</h3>
-</body>
-</html>" > /var/www/html/404.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Peppa Pig
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /var/www/html/index.html
 
 printf %s "server {
     listen 80;
@@ -32,9 +30,8 @@ printf %s "server {
     add_header X-Served-By $HOSTNAME;
     root /var/www/html;
     index index.html;
-    location /hbnb_static {
+    location /hbnb_static/ {
         alias /data/web_static/current;
-        index index.html index.htm;
     }
     location /redirect_me {
         return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
